b5ee81442913a61d7ba957c5df17ad570e78eea3
Tom Butler <broadwaytb@aol.com>
2025-10-27 18:36:02 -0500

    Initial Commit

---
+++ WolfmoreGolf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WolfmoreGolf
+//
+//  Created by Tom BUTLER on 10/27/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
